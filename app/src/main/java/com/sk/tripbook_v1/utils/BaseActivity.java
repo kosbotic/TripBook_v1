cab91e822c9bad5748c3ab768cbc57ae3f3a3d23
kosbotic <kosbotic@gmail.com>
2018-08-14 21:00:48 -0400

    Create basic packages and baseActivity

---
+++ app/src/main/java/com/sk/tripbook_v1/utils/BaseActivity.java
@@ -0,0 +1,20 @@
+package com.sk.tripbook_v1.utils;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public void addFragmentToActivity (FragmentManager fragmentManager,
+                                       Fragment fragment,
+                                       int frameId,
+                                       String tag) {
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(frameId, fragment, tag);
+        transaction.commit();
+    }
+}
